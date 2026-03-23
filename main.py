d6105c3573f4db8adb2e7e03d51922ab3f5ee3ee
yanfan <ewan4434@gmail.com>
2026-03-23 00:01:31 -0700

    DB table setup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from database import engine, Base
+import models
+
+# Create database table if not already existed
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
